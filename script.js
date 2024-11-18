31ca434501b5110a09f3bf359f8e54264fc17f92
CarolineOlivier <linepro.olivier@gmail.com>
2024-11-18 13:01:54 +0100

    Ajout de la fonctionnalité 1-bis : incrémentation des clics sur le footer

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Sélectionner l'élément footer
 const footer = document.querySelector('footer');
 
+// Déclarer une variable compteur pour compter les clics
+let clickCount = 0;
+
 // Ajouter un écouteur d'événement pour détecter le clic
 footer.addEventListener('click', function() {
-  console.log("clique");
+  // Incrémenter le compteur de clics
+  clickCount++;
+  
+  // Afficher le nombre de clics en console
+  console.log(`clic numéro ${clickCount}`);
 });
+PeriodicWave
