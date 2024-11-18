8f642a17bd1454285755e65527706e2724db7f7a
CarolineOlivier <linepro.olivier@gmail.com>
2024-11-18 12:54:47 +0100

    Initial commit with event detection functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sélectionner l'élément footer
+const footer = document.querySelector('footer');
+
+// Ajouter un écouteur d'événement pour détecter le clic
+footer.addEventListener('click', function() {
+  console.log("clique");
+});
